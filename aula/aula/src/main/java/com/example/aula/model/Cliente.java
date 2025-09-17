6df17afd74f789454ba8464154b3382bb8fb2f8a
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2025-09-17 20:29:09 -0300

    Finalizando

--- aula/aula/src/main/java/com/example/aula/model/Cliente.java
+++ aula/aula/src/main/java/com/example/aula/model/Cliente.java
@@ -1,13 +1,7 @@
 package com.example.aula.model;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
-@NoArgsConstructor
-@AllArgsConstructor
-@Data
 @Entity
 @Table(name = "tab_cliente")
 public class Cliente {
@@ -21,5 +15,36 @@ public class Cliente {
     @Column(nullable = false)
     private String email;
 
+    public Cliente() {
+    }
 
+    public Cliente(String email, Long id, String nome) {
+        this.email = email;
+        this.id = id;
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 }
