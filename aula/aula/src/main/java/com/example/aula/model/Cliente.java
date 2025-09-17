9318d79df4ae23a48b1f836c70ca8acf7c04ced0
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2025-09-17 20:14:40 -0300

    Finalizando

---
+++ aula/aula/src/main/java/com/example/aula/model/Cliente.java
@@ -0,0 +1,25 @@
+package com.example.aula.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+@Table(name = "tab_cliente")
+public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+    @Column(nullable = false)
+    private String email;
+
+
+}
